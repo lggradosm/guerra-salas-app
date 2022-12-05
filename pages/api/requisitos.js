090b75782e56280d9e47175e5b4f3000a1a5ac57
lggradosm <gabrielgradosmachado@gmail.com>
2022-12-05 00:22:47 -0500

    NEW: requisitos

--- pages/api/requisitos.js
+++ pages/api/requisitos.js
@@ -1,4 +1,4 @@
-import { requisitosList } from "../../dummy/requisitos";
-export default function handler(req, res) {
+import { requisitosList } from "../../src/requisitos";
+export default async function handler(req, res) {
   res.status(200).json(requisitosList);
 }
